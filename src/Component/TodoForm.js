6380c084b7001dc56c11b186c2ebe365401be9cf
Oluwaseun <144266502+Fooagsmart@users.noreply.github.com>
2023-11-26 03:57:46 +0100

    todo wrapper

--- src/Component/TodoForm.js
+++ src/Component/TodoForm.js
@@ -1,11 +1,24 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 export const TodoForm = () => {
-    // const [value, setValue] = useState{""}
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(value);
+  };
   return (
-    <form className='TodoForm'>
-        <input type='text' className='todo-input' placeholder='What are your plans today? ' onChange={(e) => console.log(e.target.value)}/>
-        <button type='submit' className='todo-btn'>Add Task</button>
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        placeholder="What are your plans today? "
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn">
+        Add Task
+      </button>
     </form>
-  )
-}
+  );
+};
