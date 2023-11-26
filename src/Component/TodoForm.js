e3f75a880696bd481580fe89ef600d3927f2656d
Oluwaseun <144266502+Fooagsmart@users.noreply.github.com>
2023-11-26 03:34:26 +0100

    ceck console if its working

--- src/Component/TodoForm.js
+++ src/Component/TodoForm.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const TodoForm = () => {
+    // const [value, setValue] = useState{""}
   return (
     <form className='TodoForm'>
-        <input type='text' className='todo-input' placeholder='What are your plans today? '/>
+        <input type='text' className='todo-input' placeholder='What are your plans today? ' onChange={(e) => console.log(e.target.value)}/>
         <button type='submit' className='todo-btn'>Add Task</button>
     </form>
   )
